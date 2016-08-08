aa2ae1162268a2dc35d8a3a2281c0f59dd14654d
hucule <hucule@lackmail.ru>
2016-08-08 16:26:23 -0700

    x

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-sudo apt-get update -qq
-sudo apt-get upgrade -qq
-sudo apt-get install python python-pip
+apt-get update -qq
+apt-get upgrade -qq
+apt-get install python python-pip
 pip install requests
