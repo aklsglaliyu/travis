e8337182a8a03ab4c2f37939e6c4efed88318495
hucule <hucule@lackmail.ru>
2016-08-08 17:12:02 -0700

    x

--- build.py
+++ build.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 
-import sys
+import os
 
-print('hello world')
-print(sys.version)
-print(sys.version_info)
+print(os.getenv('TRAVIS_BUILD_DIR'))
+print(os.getenv('TRAVIS_BUILD_ID'))
+print(os.getenv('TRAVIS_TAG'))
+print(os.getenv('TRAVIS_REPO_SLUG'))
+print(os.getenv('TRAVIS_JOB_ID'))
+print(os.getenv('TRAVIS_JOB_NUMBER'))
